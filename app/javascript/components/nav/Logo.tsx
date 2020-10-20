7c7d5a8f731c797ea4e7583c1456c50ffdb09100
Lukas Fülling <lukas@k40s.net>
2020-10-20 19:49:14 +0200

    add basic styles, add svg logo

--- app/javascript/components/nav/Logo.tsx
+++ app/javascript/components/nav/Logo.tsx
@@ -1,10 +1,13 @@
 import React, {FunctionComponent} from 'react';
 import logo from '../../../assets/images/logo.svg'
+import I18n from "i18n-js";
 
 type Props = {}
 
 const Logo: FunctionComponent<Props> = () => (
-    <i className={'logo'}>A</i>
-); //TODO: add better a11y
+    <div className={'logo'}>
+        <img src={logo} alt={I18n.t('nav.alt_logo')}/>
+    </div>
+);
 
 export default Logo;
