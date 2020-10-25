26173dc06cb481fd81ccd8d3c2eb41d151e73763
Lukas Fülling <lukas@k40s.net>
2020-10-25 02:13:47 +0200

    improve footer

--- app/javascript/components/Footer.tsx
+++ app/javascript/components/Footer.tsx
@@ -1,14 +1,27 @@
 import React, {FunctionComponent} from 'react';
 import User from "../model/user";
+import I18n from "i18n-js";
+import {Link} from "react-router-dom";
 
 type Props = {
     account: User | undefined,
 }
 
-const Footer: FunctionComponent<Props> = ({account}: Props) => (
-    <div id={'footer'}>
-        &copy; 2020 lolPix {account && '- logged in as ' + account.username}
-    </div>
-);
+const Footer: FunctionComponent<Props> = ({account}: Props) => {
+    const accountFragment = (
+        <p className={'secondary'}>
+            {'logged in as ' + account.username}
+            &nbsp;
+            <Link to={'/logout'}>({I18n.t('ui.footer.logout')})</Link>
+        </p>
+    );
+
+    return (
+        <div id={'footer'}>
+            <p>&copy; 2020 lolPix</p>
+            {account && accountFragment}
+        </div>
+    );
+};
 
 export default Footer;
