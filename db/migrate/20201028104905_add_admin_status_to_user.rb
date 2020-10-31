0885b69e2ec43dc07cbf4d179cf63049509bb12d
Lukas Fülling <lukas@k40s.net>
2020-10-31 01:52:46 +0100

    fix lint errors

--- db/migrate/20201028104905_add_admin_status_to_user.rb
+++ db/migrate/20201028104905_add_admin_status_to_user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Migration that adds the admin field to the user class.
 class AddAdminStatusToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :admin, :boolean
--- db/migrate/20201030233019_add_jwt_secret_to_user.rb
+++ db/migrate/20201030233019_add_jwt_secret_to_user.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Migration that adds a new field to the users column.
 class AddJwtSecretToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :jwts, :string
