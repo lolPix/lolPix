bc0089a65dd8f3efda66f6153b723c0fbaa45820
Lukas Fülling <lukas@k40s.net>
2020-10-31 05:07:34 +0100

    prevent non-admins from accessing admin ui

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,6 +3,7 @@
 # Controller for admin stuff...
 class AdminController < ApplicationController
   before_action :logged_in_user, only: %i[index reports report]
+  before_action :ensure_admin, only: %i[index reports report]
   before_action :find_report_for_id_param, only: %i[report]
   helper_method :find_reports, :find_user_for_report, :find_post_for_report
 
@@ -14,6 +15,14 @@ class AdminController < ApplicationController
 
   def find_reports
     Report.all
+  end
+
+  def ensure_admin
+    if @user.nil?
+      head :unauthorized, location: '/'
+    elsif !@user.admin
+      head :forbidden, location: '/'
+    end
   end
 
   def find_user_for_report(report)
