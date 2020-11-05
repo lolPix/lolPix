7a86492f7bcf6ae4727ada2351e9751402cc3ca6
Lukas Fülling <lukas@k40s.net>
2020-11-05 22:46:07 +0100

    get it to work

--- app/javascript/components/ssr/ServersideApp.tsx
+++ app/javascript/components/ssr/ServersideApp.tsx
@@ -1,12 +1,10 @@
 import React, {FunctionComponent} from "react";
-import {StaticRouter,} from 'react-router-dom';
+import {StaticRouter} from 'react-router-dom';
 import NavBar from "../nav/NavBar";
 import User from "../../model/User";
 import Footer from "../Footer";
-import {ToastProvider} from 'react-toast-notifications'
 import {createMemoryHistory} from "history";
 import Routes from "../../base/Routes";
-
 
 type Props = {
     account: User | undefined,
@@ -17,17 +15,15 @@ const ServersideApp: FunctionComponent<Props> = ({account, url}: Props) => {
     const pathname = new URL(url).pathname;
     const history = createMemoryHistory({initialEntries: [pathname]});
     return (
-        <ToastProvider>
-            <div className="wrapper">
-                <StaticRouter history={history} location={pathname}>
-                    <NavBar showLogo={true} account={account}/>
-                    <div className="content">
-                        <Routes account={account}/>
-                    </div>
-                    <Footer account={account}/>
-                </StaticRouter>
-            </div>
-        </ToastProvider>
+        <div className="wrapper">
+            <StaticRouter history={history} location={pathname}>
+                <NavBar showLogo={true} account={account}/>
+                <div className="content">
+                    <Routes account={account}/>
+                </div>
+                <Footer account={account}/>
+            </StaticRouter>
+        </div>
     );
 };
 
